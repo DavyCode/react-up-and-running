f61dd3f8a4e2fe2c5ad27438ba6fadac8f8fe502
Davy'd <pdaswift3009@gmail.com>
2017-09-25 19:55:32 +0100

    pull dom into react component

--- public/app.jsx
+++ public/app.jsx
@@ -1,7 +1,16 @@
-
+var Greeter = React.createClass({
+   render : function () {
+      return (
+           <div>
+               <h1> Hello Reacting...</h1>    
+           </div> 
+      );
+   }
+});
 
 
 ReactDOM.render(
-      <h1> Hello Reacting</h1>,
+      // <h1> Hello Reacting</h1>,
+      <Greeter/>,
       document.getElementById('app')
      );
